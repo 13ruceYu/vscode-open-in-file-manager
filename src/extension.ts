3f755ee63e8efb713e9e7beff6affbfb64379566
13ruceYu <bruceyuhongbo@gmail.com>
2025-01-09 14:13:59 +0800

    feat: dev

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,51 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-open-in-file-manager" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('vscode-open-in-file-manager.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-open-in-file-manager!');
-	});
-
-	context.subscriptions.push(disposable);
+function getFileManagerName() {
+  switch (process.platform) {
+    case 'darwin': // macOS
+      return 'Finder';
+    case 'win32': // Windows
+      return 'File Explorer';
+    case 'linux': // Linux
+      return 'File Manager';
+    default:
+      return 'File Manager';
+  }
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function activate(context: vscode.ExtensionContext) {
+  const fileManagerName = getFileManagerName();
+
+  const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 0);
+  statusBarItem.text = "$(file-symlink-directory)";
+  statusBarItem.tooltip = `Reveal Workspace in ${fileManagerName}`;
+  statusBarItem.command = "extension.revealWorkspaceInFileManager";
+  statusBarItem.show();
+
+  context.subscriptions.push(statusBarItem);
+
+  const revealCommand = vscode.commands.registerCommand("extension.revealWorkspaceInFileManager", () => {
+    const workspaceFolders = vscode.workspace.workspaceFolders;
+
+    if (!workspaceFolders || workspaceFolders.length === 0) {
+      vscode.window.showErrorMessage('No workspace folder is open.');
+      return;
+    }
+
+    const workspaceUri = workspaceFolders[0].uri;
+
+    vscode.commands.executeCommand('revealFileInOS', workspaceUri).then(
+      () => {
+        console.log(`Revealed workspace folder in ${fileManagerName}`);
+      },
+      (err) => {
+        console.error(`Failed to reveal folder in ${fileManagerName}:`, err);
+        vscode.window.showErrorMessage(`Failed to reveal workspace folder in ${fileManagerName}.`);
+      }
+    );
+  });
+
+  context.subscriptions.push(revealCommand);
+}
+
+export function deactivate() { }
